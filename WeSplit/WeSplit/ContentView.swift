57b7a033c92e52ae4dff2ae0f241d720fd6e6949
maegan <maegan@maeganwilson.com>
2021-01-19 19:28:31 -0600

    Day 18 done

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -18,6 +18,15 @@ struct ContentView: View {
 
         return amountPerPerson
     }
+    var totalCheck: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
     
     var body: some View {
         NavigationView{
@@ -38,8 +47,11 @@ struct ContentView: View {
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 }
-                Section(header: Text("Each person should pay")) {
+                Section(header: Text("Amount Per Person")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Check Total")) {
+                    Text("$\(totalCheck, specifier: "%.2f")")
                 }
             }.navigationBarTitle("WeSplit")
         }
