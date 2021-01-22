c2e3b37c4ad18c915ba07eee236f62c5d8eea508
maegan <maegan@maeganwilson.com>
2021-01-21 20:47:32 -0600

    Day 20 done

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = "2"
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(Int(numberOfPeople) ?? 1 + 2)
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
@@ -34,11 +34,8 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
                 }
                 Section(header: Text("How much tip do you want to leave?")){
                     Picker("Tip percentage", selection: $tipPercentage) {
@@ -53,8 +50,17 @@ struct ContentView: View {
                 Section(header: Text("Check Total")) {
                     Text("$\(totalCheck, specifier: "%.2f")")
                 }
+                Button("Done") {
+                                UIApplication.shared.endEditing() // Call to dismiss keyboard
+                            }
             }.navigationBarTitle("WeSplit")
         }
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
