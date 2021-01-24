420b65ff70373981c6662dfa1a07f96a7e87e785
maegan <maegan@maeganwilson.com>
2021-01-24 12:26:21 -0600

    Day 23

---
+++ day23/day23/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  day23
+//
+//  Created by Maegan Wilson on 1/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .padding()
+            .background(Color.red)
+            .padding()
+            .background(Color.blue)
+            .padding()
+            .background(Color.green)
+            .padding()
+            .background(Color.yellow)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day23/day23/day23App.swift
@@ -0,0 +1,17 @@
+//
+//  day23App.swift
+//  day23
+//
+//  Created by Maegan Wilson on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct day23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
